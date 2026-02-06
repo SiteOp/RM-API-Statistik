1e58624e278356e7d858efaefb5ef1afedaae7fc
Richard Gebhard <info@routes-manager.de>
2026-02-06 08:58:13 +0100

    Update rm_statistik.php
    Erweiterung der ausgelieferten Daten

--- rm_statistik.php
+++ rm_statistik.php
@@ -1,26 +1,25 @@
 <?php
 /**
- * jBackend tags plugin for Joomla
+ * jBackend rm_statistik plugin for Joomla
  *
  * @author selfget.com (info@selfget.com)
  * @package jBackend
  * @copyright Copyright 2015
  * @license GNU Public License
- * @version 1.0.0
+ * @version 2.0.0
  * @link http://www.selfget.com
- * @version 1.0.0
  */
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
-use \Joomla\Utilities\ArrayHelper;
-
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Log\Log;
 
 class plgJBackendRm_Statistik extends JPlugin
 {
-  public function __construct(& $subject, $config)
+  public function __construct(&$subject, $config)
   {
     parent::__construct($subject, $config);
     $this->loadLanguage();
@@ -28,22 +27,17 @@ class plgJBackendRm_Statistik extends JPlugin
 
   /**
    * This is the function to generate plugin specific errors
-   * The error response is an array with the following structure:
-   *    array(
-   *     'status' => 'ko',
-   *     'error_code' => $errorCode,
-   *     'error_description' => <short error description>
-   *    )
-   *
-   * @param  string  $errorCode  The error code to generate
-   *
-   * @return  array  The response of type error to return
+   * 
+   * @param string $errorCode The error code to generate
+   * @return array The response of type error to return
    */
-  public static function generateError($errorCode)
+  private function generateError($errorCode)
   {
-    $error = array();
-    $error['status'] = 'ko';
-    $error['error_code'] = $errorCode;
+    $error = array(
+      'status' => 'ko',
+      'error_code' => $errorCode
+    );
+    
     switch($errorCode) {
       case 'REQ_NVAL':
         $error['status_code'] = 405;
@@ -53,228 +47,294 @@ class plgJBackendRm_Statistik extends JPlugin
         $error['status_code'] = 400;
         $error['error_description'] = 'Action not specified';
         break;
+      case 'DB_ERROR':
+        $error['status_code'] = 500;
+        $error['error_description'] = 'Database error occurred';
+        break;
+      default:
+        $error['status_code'] = 500;
+        $error['error_description'] = 'Unknown error';
     }
+    
     return $error;
   }
 
-
   /**
-   * Anzahl Routen
+   * Sanitize table name to prevent SQL injection
+   * 
+   * @param string $tableName The table name to sanitize
+   * @return string The sanitized table name
    */
-  public static function getRoutesTotal() {
-    $app = Factory::getApplication();
-    $routes_total    = $app->input->get('routes_total', 0, 'BOOL');
-
-      if(1 == $routes_total) {
-        $db = Factory::getDbo();
-
-        $query = $db->getQuery(true);
-        $query->select(array('COUNT(*)'))
-              ->from('#__act_route')
-              ->where('state IN(1,-1)');
-
-        $db->setQuery($query);
-        $result = $db->loadResult();
-      }
-      else {
-        $result = 0;
-      }
-  
-    return $result;
+  private function sanitizeTableName($tableName)
+  {
+    // Only allow alphanumeric characters and underscores
+    return preg_replace('/[^a-zA-Z0-9_]/', '', $tableName);
   }
 
-
-  
   /**
-   * Anzahl Kommentare
+   * Get total number of routes
+   * 
+   * @return int Number of routes or 0 on error
    */
-  public static function getCommentsTotal() {
-
+  private function getRoutesTotal()
+  {
     $app = Factory::getApplication();
-    $comments_total    = $app->input->get('comments_total', 0, 'BOOL');
+    $routes_total = $app->input->get('routes_total', 0, 'BOOL');
 
-      if(1 == $comments_total) {
-        $db = Factory::getDbo();
+    if ($routes_total != 1) {
+      return 0;
+    }
 
-        $query = $db->getQuery(true);
-        $query->select(array('COUNT(c.id)'))
-              ->from('#__act_comment AS c')
-              ->join('LEFT', '#__act_route AS r ON r.id = c.route')
-              ->where('c.state IN(1,-1)');
+    try {
+      $db = Factory::getDbo();
+      $query = $db->getQuery(true)
+        ->select('COUNT(*)')
+        ->from($db->quoteName('#__act_route'))
+        ->where($db->quoteName('state') . ' IN (1, -1)');
 
-        $db->setQuery($query);
-        $result = $db->loadResult();
-      }
-      else {
-        $result = 0;
-      }
-
-    return $result;
-  }
-
-
-  public static function getNewRoutesTotal(){
-
-    $app = Factory::getApplication();
-    $new_routes_total    = $app->input->get('new_routes_total', 0, 'UINT');
-
-      if(1 == $new_routes_total) {
-
-        $db = Factory::getDbo();
-        $params    = JComponentHelper::getParams('com_act');
-        $newRouteDateRange  = $params['newroutedaterange'];
-
-        $query = $db->getQuery(true);
-        $query->select('COUNT(CASE WHEN a.setterdate > DATE_SUB( NOW(), INTERVAL '.$newRouteDateRange.' DAY ) then 1 ELSE NULL END) as  newroutes')
-              ->from('#__act_route AS a')
-              ->where('a.state IN(1,-1)')
-              ->where('a.hidden != 1');
-              
-        $db->setQuery($query);
-        $result = $db->loadResult();
-      }
-        else {
-          $result = 0;
-      }
-      return $result;
-  }
-  
-
-  /**
-   * Kommentare
-   */
-  public function getComments(){
-
-    $app = Factory::getApplication();
-    $limit    = $app->input->get('limit_comments', 0, 'UINT');
-
-      if($limit >0) {
-
-        $db	   = Factory::getDBO();
-        $query = $db->getQuery(true);
-
-        $query->select(array('c.comment','c.stars','g.uiaa', 'c.route AS route_id', 'r.name AS routename'))
-              ->from('#__act_comment AS c')
-              ->join('LEFT', '#__act_grade AS g ON g.id = c.myroutegrade')
-              ->join('LEFT', '#__act_route AS r ON r.id = c.route')
-              ->where('c.state = 1')
-              ->where('c.comment != "" ')
-              ->order('c.created DESC')
-              ->setLimit($limit); 
-
-        $db->setQuery($query);
+      $db->setQuery($query);
+      return (int) $db->loadResult();
       
-        return $db->loadAssocList();
-      }
-    return 0;
+    } catch (Exception $e) {
+      Log::add('Error in getRoutesTotal: ' . $e->getMessage(), Log::ERROR, 'plg_jbackend_rm_statistik');
+      return 0;
+    }
   }
 
-
-
   /**
-   * Routen
+   * Get total number of comments
+   * 
+   * @return int Number of comments or 0 on error
    */
-  public function getRoutes() {
-
+  private function getCommentsTotal()
+  {
     $app = Factory::getApplication();
-    $limit    = $app->input->get('limit_routes', 0, 'UINT');
+    $comments_total = $app->input->get('comments_total', 0, 'BOOL');
 
-      if($limit >0) {
+    if ($comments_total != 1) {
+      return 0;
+    }
+
+    try {
+      $db = Factory::getDbo();
+      $query = $db->getQuery(true)
+        ->select('COUNT(' . $db->quoteName('c.id') . ')')
+        ->from($db->quoteName('#__act_comment', 'c'))
+        ->join('LEFT', $db->quoteName('#__act_route', 'r') . ' ON ' . $db->quoteName('r.id') . ' = ' . $db->quoteName('c.route'))
+        ->where($db->quoteName('c.state') . ' IN (1, -1)');
+
+      $db->setQuery($query);
+      return (int) $db->loadResult();
       
-        $db	   = Factory::getDBO();
-        $query = $db->getQuery(true);
-
-        $query->select(array('r.id',
-                            'r.name', 
-                            's.settername', 
-                            'r.settergrade', 
-                            'g.uiaa',
-                            'IFNULL(round(t.avg_stars, 1), 0) AS rating',
-                            'r.setterdate'))
-              ->from('#__act_route AS r')
-              ->join('LEFT', '#__act_setter AS s ON s.id=r.setter')
-              ->join('LEFT', '#__act_trigger_calc AS t ON t.id = r.id')
-              ->join('LEFT', '#__act_grade AS g ON g.id = t.calc_grade_round')
-              ->where('r.state = 1')
-              ->order('r.setterdate DESC')
-              ->setLimit($limit); 
-
-        $db->setQuery($query);
-
-        return $db->loadAssocList();
-      }
-      else { 
-        return 0;
-      }
+    } catch (Exception $e) {
+      Log::add('Error in getCommentsTotal: ' . $e->getMessage(), Log::ERROR, 'plg_jbackend_rm_statistik');
+      return 0;
+    }
   }
 
   /**
-   * Zusammenfügen
+   * Get total number of new routes
+   * 
+   * @return int Number of new routes or 0 on error
    */
-  public function actionStatistik(&$response, &$status = null) {
-    $response['data'] = array('route' =>  self::getRoutes(),
-                              'comment' => self::getComments(),
-                              'routes_total' => self::getRoutesTotal(),
-                              'comments_total' => self::getCommentsTotal(),
-                              'new_routes_total' => self::getNewRoutesTotal()
-                   );
+  private function getNewRoutesTotal()
+  {
+    $app = Factory::getApplication();
+    $new_routes_total = $app->input->get('new_routes_total', 0, 'UINT');
 
-    // Erstellen der Antwort
-    $response['status'] = 'ok';
-    return true;
+    if ($new_routes_total != 1) {
+      return 0;
+    }
+
+    try {
+      $params = ComponentHelper::getParams('com_act');
+      $newRouteDateRange = (int) $params->get('newroutedaterange', 7);
+
+      $db = Factory::getDbo();
+      $query = $db->getQuery(true)
+        ->select('COUNT(CASE WHEN ' . $db->quoteName('a.setterdate') . ' > DATE_SUB(NOW(), INTERVAL ' . $newRouteDateRange . ' DAY) THEN 1 ELSE NULL END) AS newroutes')
+        ->from($db->quoteName('#__act_route', 'a'))
+        ->where($db->quoteName('a.state') . ' IN (1, -1)')
+        ->where($db->quoteName('a.hidden') . ' != 1');
+
+      $db->setQuery($query);
+      return (int) $db->loadResult();
+      
+    } catch (Exception $e) {
+      Log::add('Error in getNewRoutesTotal: ' . $e->getMessage(), Log::ERROR, 'plg_jbackend_rm_statistik');
+      return 0;
+    }
   }
 
   /**
-   * Fulfills requests for tags module
+   * Get comments with route information
+   * 
+   * @return array|null Array of comments or null on error
+   */
+  private function getComments()
+  {
+    $app = Factory::getApplication();
+    $limit = $app->input->get('limit_comments', 0, 'UINT');
+
+    if ($limit <= 0) {
+      return null;
+    }
+
+    try {
+      $params = ComponentHelper::getParams('com_act');
+      $grade_table = $this->sanitizeTableName($params->get('grade_table', 'act_grade'));
+
+      $db = Factory::getDbo();
+      $query = $db->getQuery(true)
+        ->select([
+          $db->quoteName('c.comment'),
+          $db->quoteName('c.stars'),
+          $db->quoteName('cg.grade', 'c_grade'),
+          $db->quoteName('c.route', 'route_id'),
+          $db->quoteName('r.name', 'routename'),
+          $db->quoteName('vr.grade', 'my_grade')
+        ])
+        ->from($db->quoteName('#__act_comment', 'c'))
+        ->join('LEFT', $db->quoteName('#__act_trigger_calc', 't') . ' ON ' . $db->quoteName('t.id') . ' = ' . $db->quoteName('c.route'))
+        ->join('LEFT', $db->quoteName('#__act_route', 'r') . ' ON ' . $db->quoteName('r.id') . ' = ' . $db->quoteName('c.route'))
+        ->join('LEFT', $db->quoteName('#__' . $grade_table, 'cg') . ' ON ' . $db->quoteName('cg.id_grade') . ' = ' . $db->quoteName('t.calc_grade_round'))
+        ->join('LEFT', $db->quoteName('#__' . $grade_table, 'vr') . ' ON ' . $db->quoteName('vr.id_grade') . ' = ' . $db->quoteName('c.myroutegrade'))
+        ->where($db->quoteName('c.state') . ' = 1')
+        ->where($db->quoteName('c.comment') . ' != ' . $db->quote(''))
+        ->order($db->quoteName('c.created') . ' DESC')
+        ->setLimit($limit);
+
+      $db->setQuery($query);
+      return $db->loadAssocList();
+      
+    } catch (Exception $e) {
+      Log::add('Error in getComments: ' . $e->getMessage(), Log::ERROR, 'plg_jbackend_rm_statistik');
+      return null;
+    }
+  }
+
+  /**
+   * Get routes with all related information
+   * 
+   * @return array|null Array of routes or null on error
+   */
+  private function getRoutes()
+  {
+    $app = Factory::getApplication();
+    $limit = $app->input->get('limit_routes', 0, 'UINT');
+
+    if ($limit <= 0) {
+      return null;
+    }
+
+    try {
+      $params = ComponentHelper::getParams('com_act');
+      $grade_table = $this->sanitizeTableName($params->get('grade_table', 'act_grade'));
+
+      $db = Factory::getDbo();
+      $query = $db->getQuery(true)
+        ->select([
+          $db->quoteName('r.id'),
+          $db->quoteName('r.name'),
+          $db->quoteName('s.settername'),
+          $db->quoteName('vr.grade', 'settergrade'),
+          $db->quoteName('cg.grade', 'c_grade'),
+          'IFNULL(ROUND(' . $db->quoteName('t.avg_stars') . ', 1), 0) AS ' . $db->quoteName('rating'),
+          $db->quoteName('r.setterdate'),
+          $db->quoteName('c.color'),
+          $db->quoteName('sec.sector', 'sector_name')
+        ])
+        ->from($db->quoteName('#__act_route', 'r'))
+        ->join('LEFT', $db->quoteName('#__act_setter', 's') . ' ON ' . $db->quoteName('s.id') . ' = ' . $db->quoteName('r.setter'))
+        ->join('LEFT', $db->quoteName('#__act_trigger_calc', 't') . ' ON ' . $db->quoteName('t.id') . ' = ' . $db->quoteName('r.id'))
+        ->join('LEFT', $db->quoteName('#__' . $grade_table, 'cg') . ' ON ' . $db->quoteName('cg.id_grade') . ' = ' . $db->quoteName('t.calc_grade_round'))
+        ->join('LEFT', $db->quoteName('#__' . $grade_table, 'vr') . ' ON ' . $db->quoteName('vr.id_grade') . ' = ' . $db->quoteName('r.settergrade'))
+        ->join('LEFT', $db->quoteName('#__act_color', 'c') . ' ON ' . $db->quoteName('c.id') . ' = ' . $db->quoteName('r.color'))
+        ->join('LEFT', $db->quoteName('#__act_line', 'l') . ' ON ' . $db->quoteName('l.id') . ' = ' . $db->quoteName('r.line'))
+        ->join('LEFT', $db->quoteName('#__act_sector', 'sec') . ' ON ' . $db->quoteName('sec.id') . ' = ' . $db->quoteName('l.sector'))
+        ->where($db->quoteName('r.state') . ' = 1')
+        ->order($db->quoteName('r.setterdate') . ' DESC')
+        ->setLimit($limit);
+
+      $db->setQuery($query);
+      return $db->loadAssocList();
+      
+    } catch (Exception $e) {
+      Log::add('Error in getRoutes: ' . $e->getMessage(), Log::ERROR, 'plg_jbackend_rm_statistik');
+      return null;
+    }
+  }
+
+  /**
+   * Combine all statistics data
+   * 
+   * @param array $response Response array to populate
+   * @param mixed $status Status object
+   * @return bool True on success
+   */
+  private function actionStatistik(&$response, &$status = null)
+  {
+    try {
+      $response['data'] = [
+        'route' => $this->getRoutes(),
+        'comment' => $this->getComments(),
+        'routes_total' => $this->getRoutesTotal(),
+        'comments_total' => $this->getCommentsTotal(),
+        'new_routes_total' => $this->getNewRoutesTotal()
+      ];
+
+      $response['status'] = 'ok';
+      return true;
+      
+    } catch (Exception $e) {
+      Log::add('Error in actionStatistik: ' . $e->getMessage(), Log::ERROR, 'plg_jbackend_rm_statistik');
+      $response = $this->generateError('DB_ERROR');
+      return false;
+    }
+  }
+
+  /**
+   * Fulfills requests for rm_statistik module
    *
-   * @param   object    $module      The module invoked (this is the same of onRequest<Module>)
-   * @param   object    $response    The response generated
-   * @param   object    $status      The boundary conditions (e.g. authentication status). Useful to return additional info
-   *
-   * @return  boolean   true if there are no problems (status = ok), false in case of errors (status = ko)
+   * @param string $module The module invoked
+   * @param array $response The response generated
+   * @param mixed $status The boundary conditions
+   * @return bool True if there are no problems
    */
   public function onRequestRm_Statistik($module, &$response, &$status = null)
   {
-    if ($module !== 'rm_statistik') return true; // Check if this is the triggered module or exit
+    // Check if this is the triggered module
+    if ($module !== 'rm_statistik') {
+      return true;
+    }
 
     // Add to module call stack
-    jBackendHelper::moduleStack($status, 'rm_statistik'); // Add this request to the module stack register
+    jBackendHelper::moduleStack($status, 'rm_statistik');
 
-    // Now check the request
-    // Each request must have three params, action/module/resource
-    // action: one of the RESTful actions (e.g. GET, POST, ...)
-    // module: is the name of the module to call (jBackend plugin) (e.g. tags)
-    // resource: is the resource requested to the module (e.g. a tag for this tags module)
-    $app      = Factory::getApplication();
-    $action   = $app->input->getString('action');
+    $app = Factory::getApplication();
+    $action = $app->input->getString('action');
     $resource = $app->input->getString('resource');
-    // module already checked by jBackend before to dispatch the request,
-    // so no needs to check the module if we are here :)
 
     // Check if the action is specified
-    if (is_null($action)) 
-    {
-      $response = self::generateError('REQ_ANS'); // Action not specified
+    if (empty($action)) {
+      $response = $this->generateError('REQ_ANS');
       return false;
     }
 
-    // POST not implemented
-    if ($action != 'get') 
-    {
-      $response = self::generateError('REQ_NVAL'); // Action not supported
+    // Only GET requests are supported
+    if ($action !== 'get') {
+      $response = $this->generateError('REQ_NVAL');
       return false;
     }
 
-    // Now we can manage any supported request. If the request doesn't match any case the function return just true
-    // jBackend initializes the response to null so if no plugin matches the request the final result is still null
-    // and the exception can be managed by jBackend itself
-    switch ($resource)
-    {
+    // Handle resources
+    switch ($resource) {
       case 'list':
         return $this->actionStatistik($response, $status);
-      case 'comment':
+      
+      default:
+        // Unknown resource - let jBackend handle it
+        return true;
     }
-
-    return true;
   }
 }
